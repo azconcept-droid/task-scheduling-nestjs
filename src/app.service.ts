d1ce659ae576365ada033645a6b792397fa5ad31
Yahaya Azeez <azconcept2016@gmail.com>
2025-05-13 12:37:00 +0100

    add handletimeout and interval

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
@@ -8,8 +8,26 @@ export class AppService {
     return 'Hello World!';
   }
 
-  @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron(CronExpression.EVERY_30_SECONDS, {
+    name: 'notifications',
+    timeZone: 'Africa/Lagos',
+  })
   handleCron() {
     this.logger.debug('Called every 30 seconds');
   }
+
+  @Interval(10000)
+  handleInterval() {
+    this.logger.debug('Called every 10 seconds');
+  }
+
+  @Timeout(5000)
+  handleTimeout() {
+    this.logger.debug('Called once after 5 seconds');
+  }
+
+  // @Interval('notifications', 2500)
+  // handleInterva() {
+  //   this.logger.debug('Called every 2500 seconds');
+  // }
 }
